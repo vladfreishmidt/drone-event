8fdabad4239b517297f9721291a588968d60638d
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-05-09 16:42:02 +0300

    Added CSS

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.menu-btn'),
+  menu = document.querySelector('nav ul'),
+  exitBtn = document.querySelector('.exit-btn');
+
+menuBtn.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0)';
+
+})
+
+exitBtn.addEventListener('click', () => {
+  menu.style.transform = 'translateX(100%)';
+})
